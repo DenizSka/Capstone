78560299d5f32296b3cd6d5293166e2aa2952bc5
Deniz Skantz <denizskantz@hotmail.com>
2020-02-29 18:35:21 -0500

    adding to database

--- api_call.py
+++ api_call.py
@@ -3,7 +3,7 @@ import tweepy
 import json
 import pprint
 
-api_key = os.environ.get('MY_API_KEY')
+api_key = os.environ.get ('MY_API_KEY')
 secret_api = os.environ['MY_SECRET_API_KEY']
 access_token = os.environ['ACCESS_TOKEN']
 secret_access_token = os.environ['SECRET_ACCESS_TOKEN']
@@ -11,15 +11,16 @@ secret_access_token = os.environ['SECRET_ACCESS_TOKEN']
 # pprint.pprint(dict(os.environ), width=1)
 
 # authenticate to the service we're accessing
-auth = tweepy.OAuthHandler(api_key, secret_api)
-auth.set_access_token(access_token, secret_access_token)
+auth = tweepy.OAuthHandler (api_key, secret_api)
+auth.set_access_token (access_token, secret_access_token)
 
-api = tweepy.API(auth)
+api = tweepy.API (auth)
 handle = 'twitter'  # for example purposes; prop any handle you want!
 
 # For example, we can read our own timeline (i.e. our Twitter homepage) with:
 public_tweets = api.home_timeline()
 user = api.get_user(handle)
+
 
 # you could just print out the JSON, one tweet per line:
 # def process_or_store(tweet):
@@ -74,10 +75,10 @@ user = api.get_user(handle)
 def dump_to_json(tweets):
     tweet_list = []
     for tweet in tweets:
-        tweet_list.append(tweet._json)
+        tweet_list.append (tweet._json)
 
-    with open('api_tweet_data.json', 'w') as f:
-        json.dump(tweet_list, f, indent=2)
+    with open ('api_tweet_data.json', 'w') as f:
+        json.dump (tweet_list, f, indent=2)
 
     return tweet_list
 
@@ -89,8 +90,8 @@ tweet_stats_list = []
 
 def get_one_user():
     # opening the json file and parsing it to a dictionary
-    with open('api_tweet_data.json', 'r') as f:
-        json_ = json.load(f)
+    with open ('api_tweet_data.json', 'r') as f:
+        json_ = json.load (f)
         user = json_[0]["user"]
         text = json_[0]["text"]
         date = json_[0]["created_at"]
@@ -101,60 +102,67 @@ def get_one_user():
 
 
 # The average length of tweets (counting words).
-def ave_word_length():
+def avg_word_length():
     tweet_counts = []
-    with open('api_tweet_data.json', 'r') as f:
-        for tweet in json.load(f):
-            list_text = tweet['text'].split(' ')
+    with open ('api_tweet_data.json', 'r') as f:
+        for tweet in json.load (f):
+            list_text = tweet['text'].split (' ')
             tweet_counts.append(len(list_text))
-        tweet_stats_list.append(f"this is average length of words in a tweet {sum(tweet_counts) / len(tweet_counts)} \n")
+        tweet_stats_list.append(
+            f"this is average length of words in a tweet {sum(tweet_counts)/len(tweet_counts)} \n")
         # return f"this is average length of words in a tweet {sum(tweet_counts) / len(tweet_counts)}"
+        return round(sum(tweet_counts)/len(tweet_counts))
 
 
 # The average length of tweets (counting characters).
-def ave_tweet_length():
+def avg_tweet_length():
     tweet_counts = []
     with open('api_tweet_data.json', 'r') as f:
         for tweet in json.load(f):
             tweet_counts.append(len(tweet['text']))
-        tweet_stats_list.append(f"this is average length of characters in tweet {sum(tweet_counts) / len(tweet_counts)} \n")
+        tweet_stats_list.append(
+            f"this is average length of characters in tweet {sum(tweet_counts)/len(tweet_counts)} \n")
         # return f"this is average length of characters in tweet {sum(tweet_counts) / len(tweet_counts)}"
+        return round(sum(tweet_counts)/len(tweet_counts))
 
 
 # The average number of followers.
-def ave_follower_num():
+def avg_follower_num():
     list_followers = []
-    with open('api_tweet_data.json', 'r') as f:
+    with open ('api_tweet_data.json', 'r') as f:
         for one in json.load(f):
             list_followers.append(one['user']['followers_count'])
-        tweet_stats_list.append(f"this is average follower number {sum(list_followers) / len(list_followers)} \n")
+        tweet_stats_list.append (f"this is average follower number {sum(list_followers)/len(list_followers)} \n")
         # return f"this is average follower number {sum(list_followers) / len(list_followers)}"
+        return round(sum(list_followers)/len(list_followers))
 
 
 # The percentage of tweets that have a hashtag (#).
 def hashtag_percentage():
     tweet_count = 0
     tweets_with_hashtag = 0
-    with open('api_tweet_data.json', 'r') as f:
-        for one in json.load(f):
+    with open ('api_tweet_data.json', 'r') as f:
+        for one in json.load (f):
             tweet_count += 1
             if '#' in one['text']:
                 tweets_with_hashtag += 1
-        tweet_stats_list.append(f"percentage of hashtag {100 * tweets_with_hashtag / tweet_count} % \n")
+        tweet_stats_list.append (f"percentage of hashtag {100 * tweets_with_hashtag / tweet_count} % \n")
         # return f"percentage of hashtag {100 * tweets_with_hashtag / tweet_count} %"
+        return round(100*tweets_with_hashtag/tweet_count)
 
 
 # The percentage of tweets that have a mention (@).
 def mention_percentage():
     tweet_count = 0
     tweets_with_mentions = 0
-    with open('api_tweet_data.json', 'r') as f:
-        for one in json.load(f):
+    with open ('api_tweet_data.json', 'r') as f:
+        for one in json.load (f):
             tweet_count += 1
             if '@' in one['text']:
                 tweets_with_mentions += 1
-        tweet_stats_list.append(f"percentage of mentions {100 * tweets_with_mentions / tweet_count} % \n")
+        tweet_stats_list.append (f"percentage of mentions {100 * tweets_with_mentions / tweet_count} % \n")
         # return f"percentage of mentions {100 * tweets_with_mentions / tweet_count} %"
+        return round(100*tweets_with_mentions/tweet_count)
 
 
 # The 100 most common words.
@@ -163,38 +171,39 @@ def common_words_in_data():
     top100 = {}
     # add all the words to a list
     # if any of the words already exist than add that new work as a dictionary with its number value
-    with open('api_tweet_data.json', 'r') as f:
-        for one in json.load(f):
-            list_text = one['text'].split(' ')
+    with open ('api_tweet_data.json', 'r') as f:
+        for one in json.load (f):
+            list_text = one['text'].split (' ')
             for word in list_text:
                 if word not in words:
-                    words.append(word)
+                    words.append (word)
                 elif word in words:
-                    top100[word] = list_text.count(word)
-        sorted_dict = sorted(top100.items(), key=lambda x: x[1], reverse=True)
-        tweet_stats_list.append(f"there are to used common words {sorted_dict}\n")
+                    top100[word] = list_text.count (word)
+        sorted_dict = sorted (top100.items (), key=lambda x: x[1], reverse=True)
+        tweet_stats_list.append (f"there are to used common words {sorted_dict}\n")
         # return f"there are to used common words {sorted_dict}"
+        return sorted_dict
 
 
 # The 100 most common symbols.
 def common_symbols_in_data():
     symbols = []
     letters = 'abcdefghijklmnopqrstuvwxyz'
-    alphabet = list(letters)
+    alphabet = list (letters)
     top100 = {}
     # list_letters = []
     # add all the words to a list
     # if any of the words already exist than add that new work as a dictionary with its number value
-    with open('api_tweet_data.json', 'r') as f:
-        for one in json.load(f):
-            list_text = one['text'].split(' ')
+    with open ('api_tweet_data.json', 'r') as f:
+        for one in json.load (f):
+            list_text = one['text'].split (' ')
             # print(list_text)
-            for i in range(0, len(alphabet)):
+            for i in range (0, len (alphabet)):
                 for word in list_text:
                     # print(word)
-                    if alphabet[i] in list(word):
+                    if alphabet[i] in list (word):
                         # print("z" in ["a","b"])
-                        symbols.append(word)
+                        symbols.append (word)
                         # print(symbols)
                     # elif word in symbols:
                     #     top100[word] = list_text.count(word)
@@ -209,7 +218,8 @@ def common_symbols_in_data():
         # return f"there are to used common symbols {sorted_dict}"
 
 
-print(common_symbols_in_data())
+print (common_symbols_in_data ())
+
 
 # Percentage of tweets that use punctuation.
 
@@ -217,70 +227,74 @@ print(common_symbols_in_data())
 # The longest word in a tweet.
 def longest_word_in_data():
     longest_word = ''
-    with open('api_tweet_data.json', 'r') as f:
-        for one in json.load(f):
-            list_text = one['text'].split(' ')
+    with open ('api_tweet_data.json', 'r') as f:
+        for one in json.load (f):
+            list_text = one['text'].split (' ')
             for word in list_text:
                 if 'http' not in word and "@" not in word:
-                    if len(longest_word) < len(word):
+                    if len (longest_word) < len (word):
                         longest_word = word
-        tweet_stats_list.append(f"this is longest word '{longest_word}' \n")
+        tweet_stats_list.append (f"this is longest word '{longest_word}' \n")
         # return f"this is longest word {longest_word}"
+        return longest_word
 
 
 # Shortest word in a tweet.
 def shortest_word_in_data():
     shortest_word = 'longestwordever'
-    with open('api_tweet_data.json', 'r') as f:
-        for one in json.load(f):
-            list_text = one['text'].split(' ')
+    with open ('api_tweet_data.json', 'r') as f:
+        for one in json.load (f):
+            list_text = one['text'].split (' ')
             for word in list_text:
                 if 'http' not in word and "@" not in word:
                     if len(shortest_word) > len(word):
                         shortest_word = word
-        tweet_stats_list.append(f"this is shortest word '{shortest_word}' \n")
+        tweet_stats_list.append (f"this is shortest word '{shortest_word}' \n")
         # return f"this is shortest word {shortest_word}"
+        return shortest_word
 
 
 # What user has the most tweets in the dataset?
 def most_tweet_num_user():
     tweets_count = 0
-    with open('api_tweet_data.json', 'r') as f:
-        for one in json.load(f):
-            if int(one['user']['statuses_count']) > tweets_count:
-                tweets_count = int(one['user']['statuses_count'])
+    with open ('api_tweet_data.json', 'r') as f:
+        for one in json.load (f):
+            if int (one['user']['statuses_count']) > tweets_count:
+                tweets_count = int (one['user']['statuses_count'])
                 user = one['user']["name"]
-        tweet_stats_list.append(f"this is the user {user} with most tweets {tweets_count}\n")
+        tweet_stats_list.append (f"this is the user {user} with most tweets {tweets_count}\n")
         # return f"this is the user {user} with most tweets {tweets_count}"
+        return f" user:{user}, tweet_count: '{tweets_count}' "
 
 
 # The average number of tweets from an individual user.
-def ave_tweet_num():
+def avg_tweet_num_per_user():
     tweets_counts = []
-    with open('api_tweet_data.json', 'r') as f:
-        for one in json.load(f):
-            tweets_counts.append(one['user']['statuses_count'])
-        tweet_stats_list.append(f"this is average follower number {sum(tweets_counts) / len(tweets_counts)}\n")
+    with open ('api_tweet_data.json', 'r') as f:
+        for one in json.load (f):
+            tweets_counts.append (one['user']['statuses_count'])
+        tweet_stats_list.append (f"this is average follower number {sum(tweets_counts) / len(tweets_counts)}\n")
         # return f"this is average follower number {sum(tweets_counts) / len(tweets_counts)}"
+        return round(sum(tweets_counts)/len(tweets_counts))
 
 
 # The hour with the greatest number of tweets.
 # Write your results to a new text file.
 
 def write_to_file():
-    ave_word_length()
-    ave_tweet_length()
-    ave_follower_num()
+    avg_word_length()
+    avg_tweet_length()
+    avg_follower_num()
     hashtag_percentage()
     mention_percentage()
     common_words_in_data()
     longest_word_in_data()
     shortest_word_in_data()
     most_tweet_num_user()
-    ave_tweet_num()
-    with open("./tweet_stats.json", "w") as file_:
+    avg_tweet_num_per_user()
+    with open ("./tweet_stats.json", "w") as file_:
         for one in tweet_stats_list:
-            file_.write(one)
+            file_.write (one)
 
 
-write_to_file()
+write_to_file ()
--- database.py
+++ database.py
@@ -2,38 +2,106 @@
 # from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 # from sqlalchemy.ext.declarative import declarative_base  # we need this to import base class
 # engine = create_engine('mysql+mysqlconnector://root:Welcome_123@localhost:3306/sakila')
-# # dump of all sql thats executed
-from distutils.sysconfig import get_python_lib
-import mysql.connector
-cnx = mysql.connector.connect(user='root', password='Welcome_123',
-                              host='127.0.0.1',
-                              database='sakila')
-cursor = cnx.cursor()
 # connection = engine.connect()
 # metadata = sqlalchemy.MetaData()
 # actor = sqlalchemy.Table('actor', metadata, autoload=True, autoload_with=engine)
+
+from api_call import avg_word_length, avg_tweet_length, avg_follower_num, hashtag_percentage, \
+mention_percentage, common_words_in_data, longest_word_in_data, shortest_word_in_data, \
+most_tweet_num_user, avg_tweet_num_per_user \
+
+import mysql.connector
+from mysql.connector import errorcode
+
+cnx = mysql.connector.connect (user='root', password='Welcome_123',
+                               host='127.0.0.1',
+                               database='twitter')
+cursor = cnx.cursor ()
 
 # # Write a script that stores the data returned from the Twitter API in a SQL database.
 # # Then read that data from your database and use the code you wrote previously to analyze it.
 # # And finally, also write the results to the database.
 # # create schema twitter;
 
+TABLES = {}
+TABLES['twitter_stats'] = (
+    "CREATE TABLE `twitter_stats` ("
+    "  `id` SMALLINT NOT NULL AUTO_INCREMENT,"
+    "  `date` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,"
+    "  `avg_word_length` int(100) NOT NULL,"
+    "  `avg_tweet_length` int(100) NOT NULL,"
+    "  `hashtag_percentage` varchar(100) NOT NULL,"
+    "  `mention_percentage` varchar(100) NOT NULL,"
+    "  `longest_word_in_data` varchar(100) NOT NULL,"
+    "  `shortest_word_in_data` varchar(100) NOT NULL,"
+    "  `most_tweet_num_user` varchar(100) NOT NULL,"
+    "  `avg_tweet_num_per_user` int(20) NOT NULL,"
 
-# Base = declarative_base()
-#
-# class Twitter(Base):
-#     __tablename__ = "twitter"
-#
-#     id = Column('id', Integer, primary_key=True)
-#     tweet = Column('Tweet', String)
-#
-#
+    "  PRIMARY KEY (`id`)"
+    ") ENGINE=InnoDB")
 
-sql_select_Query = "select * from actor"
-cursor.execute(sql_select_Query)
+# TABLES['top_words'] = (
+#     "CREATE TABLE `twitter_stats` ("
+#     "  `id` SMALLINT NOT NULL AUTO_INCREMENT,"
+#     "  `date` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,"
+#     "  `common_words_in_data` varchar(1000) NOT NULL,"
+#     "  `amount of times` varchar(100) NOT NULL,"
+#     "  PRIMARY KEY (`id`)"
+#     ") ENGINE=InnoDB")
 
-for one in cursor:
-  print(one)
+def create_table(cursor):
+    for table_name in TABLES:
+        table_description = TABLES[table_name]
+    try:
+        print ("Creating table {}: ".format(table_name), end='')
+        cursor.execute(table_description)
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+            print("already exists.")
+        else:
+            print(err.msg)
+    else:
+        print("OK")
 
-cursor.close()
-cnx.close()
+
+create_table(cursor)
+
+add_tweet = ("INSERT INTO twitter_stats "
+             " (avg_word_length, avg_tweet_length, hashtag_percentage, "
+             " mention_percentage, longest_word_in_data, shortest_word_in_data, "
+             " most_tweet_num_user, avg_tweet_num_per_user)"
+             "VALUES (%(avg_word_length)s, %(avg_tweet_length)s, %(hashtag_percentage)s, "
+             " %(mention_percentage)s, %(longest_word_in_data)s, %(shortest_word_in_data)s, "
+             " %(most_tweet_num_user)s, %(avg_tweet_num_per_user)s);")
+# Insert tweet info
+data_tweets = {
+        'avg_word_length': avg_word_length(),
+        'avg_tweet_length': avg_tweet_length(),
+        'hashtag_percentage': hashtag_percentage(),
+        'mention_percentage': mention_percentage(),
+        'longest_word_in_data': longest_word_in_data(),
+        'shortest_word_in_data': shortest_word_in_data(),
+        'most_tweet_num_user': f"{most_tweet_num_user()}",
+        'avg_tweet_num_per_user': avg_tweet_num_per_user()
+    }
+
+def insert_into_table():
+    print ("inserting into table twitter_stats", end='')
+    cursor.execute(add_tweet, data_tweets)
+    # Make sure data is committed to the database
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+
+# insert_into_table()
+
+# sql_select_Query = "delete from twitter_stats"
+
+# sql_select_Query = "select * from actor"
+# cursor.execute(sql_select_Query)
+#
+# for one in cursor:
+#   print(one)
+# cnx.commit()
+# cursor.close()
+# cnx.close()
